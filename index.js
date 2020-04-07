070b6a443847c21ab5de0c008d19e1238d3af8d0
Fabio Tabah fabio.tabah@resource.com.br <Fabio Tabah fabio.tabah@resource.com.br>
2020-04-07 00:14:43 -0300

    Ready to use
    Just have node.js installed, download this repo, and double-click on
    start.bat

---
+++ index.js
@@ -0,0 +1,45 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+
+http.listen(8033, function(){
+  console.log('listening on *:8033');
+});
+
+var io = require('socket.io')(http);
+
+const users = {}
+const colors = {}
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+	  console.log("new " + name)
+	  console.log("new sid " + socket.id)
+	  users[socket.id] = name
+	  //cria cor randomica pra letra
+	  var letters = '23456789ABCD';
+	  var color = '#';
+	  for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 12)];
+	  }
+	  colors[socket.id] = color
+	  
+	  io.emit('chat message', colors[socket.id] + ';' + users[socket.id] + " CONNECTED");
+  })
+  socket.on('chat message', function(msg){
+    io.emit('chat message', colors[socket.id] + ';' + users[socket.id] + ": " + msg);
+  });
+  socket.on('disconnect', () => {
+	  if (users[socket.id] != null) {
+		socket.emit('chat message', users[socket.id] + " DISCONNECTED");
+		delete users[socket.id]
+	  }
+	  else {
+		socket.emit('chat message', "SOMEONE DISCONNECTED");
+	  }
+  })
+  
+})
